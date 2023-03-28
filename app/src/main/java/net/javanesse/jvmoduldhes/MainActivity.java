8af060388a69d667288c272ed5d14f2385fc7c8b
Javanesse <javanesse@Javanesses-MacBook-Pro.local>
2023-03-28 23:16:42 +0700

    Initial commit

---
+++ app/src/main/java/net/javanesse/jvmoduldhes/MainActivity.java
@@ -0,0 +1,96 @@
+package net.javanesse.jvmoduldhes;
+
+
+import android.app.ActionBar;
+import android.media.AudioAttributes;
+import android.media.SoundPool;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.button.MaterialButton;
+
+public class MainActivity extends AppCompatActivity {
+    private SoundPool soundPool;
+    private int soundID;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Begin FUlLSCREEN
+        View decorView = getWindow().getDecorView();
+        // Hide the status bar.
+        int uiOptions = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+        decorView.setSystemUiVisibility(uiOptions);
+        // Remember that you should never show the action bar if the
+        // status bar is hidden, so hide that too if necessary.
+//        ActionBar actionBar = getActionBar();
+//        actionBar.hide();
+
+        // End FUlLSCREEN
+
+
+
+
+        MaterialButton buttonPlay = findViewById(R.id.jvButton);
+
+        // Inisialisasi sound pool
+        AudioAttributes.Builder attrBuilder = new AudioAttributes.Builder()
+                .setUsage(AudioAttributes.USAGE_GAME)
+                .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION);
+        SoundPool.Builder builder = new SoundPool.Builder()
+                .setMaxStreams(1)
+                .setAudioAttributes(attrBuilder.build());
+        soundPool = builder.build();
+
+        // Muat suara ke dalam sound pool
+        soundID = soundPool.load(this, R.raw.nyco_dhes, 1);
+        // Njajal Nggawe Modul Dhes 16-bit 48Khz
+
+//        // Setelah suara dimuat, set listener pada tombol Play Sound
+//        buttonPlay.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                soundPool.play(soundID, 1, 1, 1, 0, 1);
+//            }
+//        });
+//
+//        // Klik menggunakan Mouse
+//        buttonPlay.performClick();
+
+
+        // Begin Touch Down Trigger Button
+        buttonPlay.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        // Kode yang ingin dijalankan saat tombol ditekan
+                        soundPool.play(soundID, 1, 1, 1, 0, 1);
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        // Kode yang ingin dijalankan saat tombol dilepas
+                        break;
+                }
+                return false;
+            }
+        });
+
+        // END Touch Down Trigger Button
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        soundPool.release(); // Jangan lupa untuk membebaskan sound pool setelah selesai menggunakan aplikasi
+    }
+}
